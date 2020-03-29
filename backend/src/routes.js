7d194097de8b6d6601f1358741af9d9525a3995b
Marlon Klagenberg <marlon.klagenberg@ilegra.com>
2020-03-29 19:44:35 -0300

    add jest and make a few tests

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { celebrate, Segments, Joi } = require('celebrate')
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
@@ -9,10 +10,39 @@ const routes = express.Router()
 routes.post('/sessions', SessionController.create)
 
 routes.get('/ongs', OngController.get)
-routes.post('/ongs', OngController.insert)
-routes.post('/incidents', IncidentController.insert)
-routes.get('/incidents', IncidentController.get)
+routes.post('/ongs', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required().min(10).max(13),
+        city: Joi.string().required(),
+        uf: Joi.string().required().length(2)
+    })
+}), OngController.insert)
+
+routes.get('/incidents', celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+        page: Joi.number()
+    })
+}), IncidentController.get)
+
 routes.get('/incidents/:id', IncidentController.get)
-routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.post('/incidents', celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required()
+    }).unknown(),
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required(),
+    })
+}), IncidentController.insert)
+
+routes.delete('/incidents/:id', celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+        id: Joi.number().required()
+    })
+}), IncidentController.delete)
 
 module.exports = routes
