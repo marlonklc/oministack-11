5836272c89ce5cbf06bed5bf33a1791c4f975f99
Marlon Klagenberg <marlon.klagenberg@ilegra.com>
2020-04-01 00:55:33 -0300

    change backend address

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://18.204.202.82:3333'
+    baseURL: 'https://alb-mkc-2131479068.us-east-1.elb.amazonaws.com'
 })
 
 export default api
